f72a4a00df47b0c9a8cc587b8d20a57a4817a822
BasVanSch <93584219+BasVanSch@users.noreply.github.com>
2024-09-21 10:25:55 +0200

    Koppeltabel en class rename

---
+++ HetFrietje/Models/Option.cs
@@ -0,0 +1,24 @@
+﻿namespace HetFrietje.Models
+{
+    public class Option
+    {
+        public int ProductOptionId { get; set; }
+        public string Name { get; set; }
+        public List<string> Values { get; set; }
+
+        public int AddValue(string value)
+        {
+            return -1;
+        }
+
+        public int UpdateValue(int index, string value)
+        {
+            return -1;
+        }
+
+        public int RemoveValue(string value)
+        {
+            return -1;
+        }
+    }
+}
--- HetFrietje/Models/ProductOption.cs
+++ HetFrietje/Models/ProductOption.cs
@@ -2,23 +2,9 @@
 {
     public class ProductOption
     {
-        public int ProductOptionId { get; private set; }
-        public string Name { get; private set; }
-        public List<string> Values { get; private set; }
-
-        public int AddValue(string value)
-        {
-            return -1;
-        }
-
-        public int UpdateValue(int index, string value)
-        {
-            return -1;
-        }
-
-        public int RemoveValue(string value)
-        {
-            return -1;
-        }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public int OptionId { get; set; }
+        public Option Option { get; set; }
     }
 }
