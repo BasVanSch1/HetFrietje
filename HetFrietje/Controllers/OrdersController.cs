05335461bfc9280ca5de91c37289d0af3e8211af
BasVanSch1 <8457098+BasVanSch1@users.noreply.github.com>
2024-10-01 11:36:22 +0200

    Eerste opzet API

---
+++ HetFrietje/Controllers/OrdersController.cs
@@ -0,0 +1,26 @@
+﻿using HetFrietje.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HetFrietje.Controllers
+{
+    public class OrdersController : Controller
+    {
+
+        private readonly DatabaseContext dbContext;
+
+        public OrdersController(DatabaseContext _context)
+        {
+            dbContext = _context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var orders = await dbContext.Orders
+                            .Include(o => o.Products)
+                            .ToListAsync();
+
+            return View(orders);
+        }
+    }
+}
--- HetFrietje/Models/Order.cs
+++ HetFrietje/Models/Order.cs
@@ -5,7 +5,7 @@
         public int OrderId { get; set; }
         public string Username { get; set; }
         public User User { get; set; }
-        public ICollection<ProductOrder>? Products { get; set; }
+        public IList<ProductOrder>? Products { get; set; }
         public OrderStatus Status { get; set; }
         public decimal TotalPrice { get; set; }
         public decimal SubtotalPrice { get; set; }
@@ -17,12 +17,36 @@
 
         private decimal CalculateTotal()
         {
-            return 0M;
+            decimal total = 0;
+
+            if (Products != null)
+            {
+                foreach (var productOrder in Products)
+                {
+                    total += CalculateTaxOfProduct(productOrder.Product) * productOrder.ProductCount + productOrder.Product.Price * productOrder.ProductCount; // tax * count + price * count
+                }
+            }
+
+            return total;
         }
 
         private decimal CalculateSubtotal()
         {
-            return 0M;
+            decimal total = 0;
+            if (Products != null)
+            {
+                foreach (var productOrder in Products)
+                {
+                    total += productOrder.Product.Price * productOrder.ProductCount;
+                }
+            }
+
+            return total;
+        }
+
+        private decimal CalculateTaxOfProduct(Product product)
+        {
+            return (product.Price / 100) * product.Tax;
         }
     }
 }
