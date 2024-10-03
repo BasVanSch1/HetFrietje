e32c19156f9d2018ff7a1277d46b47b73078c9d3
BasVanSch1 <8457098+BasVanSch1@users.noreply.github.com>
2024-10-03 19:11:35 +0200

    API Controller / Products

--- HetFrietjeAPI/Controllers/ProductsController.cs
+++ HetFrietjeAPI/Controllers/ProductsController.cs
@@ -25,14 +25,20 @@ namespace HetFrietjeAPI.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
         {
-            return await _context.Products.ToListAsync();
+            return await _context.Products
+                .Include(p => p.Categories)
+                .Include(p => p.Options)
+                .ToListAsync();
         }
 
         // GET: api/Products/5
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            var product = await _context.Products.FindAsync(id);
+            var product = await _context.Products
+                                    .Include(p => p.Categories)
+                                    .Include(p => p.Options)
+                                    .FirstOrDefaultAsync(p => p.ProductId == id);
 
             if (product == null)
             {
