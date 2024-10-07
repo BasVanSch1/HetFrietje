cf4806febec1b637fa4d56a9d06f0a3ed6e6981a
BasVanSch1 <8457098+BasVanSch1@users.noreply.github.com>
2024-10-07 12:55:07 +0200

    removed unused methods.

--- HetFrietje/Models/Option.cs
+++ HetFrietje/Models/Option.cs
@@ -6,20 +6,5 @@
         public string Name { get; set; }
         public IList<string> Values { get; set; }
         public IList<Product>? Products { get; set; } // Dit is nodig voor de many-to-many relatie.
-
-        public int AddValue(string value)
-        {
-            return -1;
-        }
-
-        public int UpdateValue(int index, string value)
-        {
-            return -1;
-        }
-
-        public int RemoveValue(string value)
-        {
-            return -1;
-        }
     }
 }
